613f176f8747d39c5a213ded621225690e0afb0e
杨威利 <jyxk@Magi.localdomain>
2018-09-24 09:16:18 +0800

    daily push

--- learning/equals/src/equals/Employee.java
+++ learning/equals/src/equals/Employee.java
@@ -26,5 +26,33 @@ public class Employee {
         return hireDay;
     }
 
-    
+    public void raiseSalary(double byPercent) {
+        double raise = salary * byPercent / 100;
+        salary += raise;
+    }
+
+    public boolean equals(Object otherObject) {
+        if (this == otherObject)
+            return true;
+
+        if (otherObject == null)
+            return false;
+
+        if (getClass() != otherObject.getClass())
+            return false;
+
+        Employee other = (Employee) otherObject;
+
+        return Objects.equals(name, other.name) && salary == other.salary && Objects.equals(hireDay, other.hireDay);
+
+    }
+
+    public int hashCode() {
+        return Objects.hash(name, salary, hireDay);
+    }
+
+    public String toString() {
+        return getClass().getName() + "[name=" + name + ", salary=" + salary + " ,hireday=" + hireDay + "]";
+    }
+
 }
--- learning/equals/src/equals/Main.java
+++ learning/equals/src/equals/Main.java
@@ -1,8 +1,32 @@
 package equals;
 
+import java.util.EnumMap;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Employee alice1 = new Employee("Alice Adams", 75000, 1987, 12, 15);
+        Employee alice2 = alice1;
+        Employee alice3 = new Employee("Alice Adams", 75000, 1987, 12, 15);
+        Employee bob = new Employee("Bob Brandson", 50000, 1989, 10, 1);
+
+        System.out.println("alice1 == alice2:" + (alice1 == alice2));
+        System.out.println("alice1 == alice3:" + (alice1 == alice3));
+        System.out.println("alice1.equalsL(alice3):" + alice1.equals(alice3));
+        System.out.println("alice1.equalsL(bob):" + alice1.equals(bob));
+
+        Manager carl = new Manager("Carl Cracker", 8000, 1987, 12, 15);
+        Manager boss = new Manager("Carl Cracker", 8000, 1987, 12, 15);
+        boss.setBonus(5000);
+
+        System.out.println(boss);
+        System.out.println(carl.equals(boss));
+        System.out.println(alice1.hashCode());
+        System.out.println(alice3.hashCode());
+        System.out.println(bob.hashCode());
+        System.out.println(carl.hashCode());
+
+
     }
 }
